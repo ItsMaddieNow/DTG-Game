fbf998ff9be1b81bad5dd8a133f5837d7096dc67
Mario Whitmore <mariolwhitmore@gmail.com>
2020-05-28 22:30:21 +1200

    Crystal emissions, particles, particle emissions, idle animations and particles functional Destroying animation and particles are WIP

---
+++ Assets/CrystalManager.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CrystalManager : MonoBehaviour
+{
+    public GameObject ParticleBurst;
+    private Animator CrystalAnimator;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        CrystalAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CreateParticleBurst()
+    {
+        Instantiate(ParticleBurst);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")){
+            CrystalAnimator.SetBool("Destroying", true);
+            print("Heck2");
+        }
+        print("Heck");
+    }
+}
