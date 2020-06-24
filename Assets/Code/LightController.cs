7827f7ad76814c48cac830ed51ff7766a69be657
Mario Whitmore <mariolwhitmore@gmail.com>
2020-06-24 18:20:28 +1200

    Dropped use of unity's animation system in favor of a custom C# based implementation for darkening the light when the player enters the water Switched from local to world space for portable torch particles for more realistic acting smoke

--- Assets/Code/LightController.cs
+++ Assets/Code/LightController.cs
@@ -2,22 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
-[RequireComponent(typeof(Animator))]
+//[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Light2D))]
 public class LightController : MonoBehaviour
 {
     
-    public Animator ThisAnimator;
+    //public Animator ThisAnimator;
+    public Light2D ThisLight;
     public int BodiesOfWater = 0;
+    [SerializeField] private float MinLight = 0;
+    [SerializeField] private float MaxLight = 0.5f;
+    [SerializeField] private float ChangeSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        ThisAnimator = this.GetComponent<Animator>();
+        ThisLight = this.GetComponent<Light2D>();
+        //ThisAnimator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ThisAnimator.SetBool("Dark", (BodiesOfWater>0));
+        if (BodiesOfWater > 0)
+        {
+            ThisLight.intensity = Mathf.Lerp(ThisLight.intensity, MinLight, ChangeSpeed/Mathf.Abs(ThisLight.intensity - MinLight) * Time.deltaTime);
+        }
+        else
+        {
+            ThisLight.intensity = Mathf.Lerp(ThisLight.intensity, MaxLight, ChangeSpeed/Mathf.Abs(ThisLight.intensity - MaxLight) * Time.deltaTime);
+        }
+        
+        //ThisAnimator.SetBool("Dark", (BodiesOfWater>0));
     }
 }
