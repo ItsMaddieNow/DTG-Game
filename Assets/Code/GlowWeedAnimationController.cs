f11f4b624cab73e1a36b9be68b6ac18f4b742e46
Mario Whitmore <mariolwhitmore@gmail.com>
2020-06-29 21:34:42 +1200

    Make Glow Weed Illuminate When the Player Comes Close

---
+++ Assets/Code/GlowWeedAnimationController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowWeedAnimationController : MonoBehaviour
+{
+    public Animator ThisAnimator;
+    public bool LightOn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThisAnimator.SetBool("Light On",LightOn);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Trigger");
+            ThisAnimator.SetBool("Player In Range",true);
+        }
+    }
+}
