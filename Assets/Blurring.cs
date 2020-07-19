d37f52b8c05b76245627cf42ba33bfff5c9bc126
mariowhitmore <36354501+mariowhitmore@users.noreply.github.com>
2020-07-20 08:28:40 +1200

    Merge

---
+++ Assets/Blurring.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Blurring : MonoBehaviour
+{
+    //public Volume ThisVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
