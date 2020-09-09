2510d6391fb21a47af9a6f7ade0ee9a2a9cc7eae
Mario Whitmore <mariolwhitmore@gmail.com>
2020-09-09 13:29:18 +1200

    ✨ Improvements To Bubble Script, New Water Shaders and Tileset

--- Assets/Code/Level Elements/Bubble.cs
+++ Assets/Code/Level Elements/Bubble.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    
+    public float PopDelay=1f;
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Water")){
+            Destroy(this.gameObject, PopDelay);
+        }
+    }
 }
